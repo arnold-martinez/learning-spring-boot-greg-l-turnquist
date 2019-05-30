6e7e363d9dec7caa05754f97cd0914438ce8fe47
Arnold Martinez <arnold.martinez.martinez.91@gmail.com>
2019-05-29 19:00:15 -0500

    Creating a service to store images

--- src/main/java/com/greglturnquist/learningspringboot/ImageRepository.java
+++ src/main/java/com/greglturnquist/learningspringboot/ImageRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 public interface ImageRepository extends PagingAndSortingRepository<Image, Long> {
 
     public Image findByName(String name);
+
 }
